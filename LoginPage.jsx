8f6b45aa63e6728a7f40e5fa2622719587c8f1c1
DeoKanKoong <rla9046@gmail.com>
2024-10-31 22:15:36 +0900

    실습2 로그인

--- LoginPage.jsx
+++ LoginPage.jsx
@@ -1,104 +1,102 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import styled from "styled-components";
+import useForm from '../hooks/use-form';
+import { validateLogin } from "./utils/validate";
 
-const LoginPage = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [passwordError, setPasswordError] = useState('');
-  const [touched, setTouched] = useState({
-    email: false,
-    password: false,
+const Login = () => {
+  const login = useForm({
+    initialValue: {
+      email: '',
+      password: '',
+    },
+    validate: validateLogin
   });
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-    if (!/\S+@\S+\.\S+/.test(e.target.value)) {
-      setEmailError('올바른 이메일 형식이 아닙니다.');
-    } else {
-      setEmailError('');
-    }
+  const handlePressLogin = () => {
+    console.log(login.values.email, login.values.password);
   };
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-    if (e.target.value.length < 8 || e.target.value.length > 16) {
-      setPasswordError('비밀번호는 8자리 이상 16자리 이하이어야 합니다.');
-    } else {
-      setPasswordError('');
-    }
-  };
-
-  const handleBlur = (field) => {
-    setTouched({ ...touched, [field]: true });
-  };
-
-  const isFormValid = email && password && !emailError && !passwordError;
+  const isFormValid = !login.errors.email && !login.errors.password && login.values.email && login.values.password;
 
   return (
-    <LoginContainer>
-      <h1>로그인</h1>
-      <Input
-        type="email"
-        value={email}
-        onChange={handleEmailChange}
-        onBlur={() => handleBlur('email')}
-        placeholder="이메일을 입력해주세요!"
-      />
-      {touched.email && emailError && <ErrorMessage>{emailError}</ErrorMessage>}
-      <Input
-        type="password"
-        value={password}
-        onChange={handlePasswordChange}
-        onBlur={() => handleBlur('password')}
-        placeholder="비밀번호를 입력해주세요!"
-      />
-      {touched.password && passwordError && <ErrorMessage>{passwordError}</ErrorMessage>}
-      <Button disabled={!isFormValid} isValid={isFormValid}>
-        로그인
-      </Button>
-    </LoginContainer>
+    <>
+      <PageContainer>
+        <MainContainer>
+          <LoginText>로그인</LoginText>
+          <StyledInput
+            type={'email'}
+            placeholder={'이메일을 입력해주세요!'}
+            {...login.getTextInputProps('email')}
+          />
+          {login.touched.email && login.errors.email && <ErrorText>{login.errors.email}</ErrorText>}
+          <StyledInput
+            type={'password'}
+            placeholder={'비밀번호를 입력해주세요!'}
+            {...login.getTextInputProps('password')}
+          />
+          {login.touched.password && login.errors.password && <ErrorText>{login.errors.password}</ErrorText>}
+          <SubmitButton
+            type={'button'}
+            onClick={handlePressLogin}
+            disabled={!isFormValid}
+            isFormValid={isFormValid}
+            value="로그인"
+          />
+        </MainContainer> 
+      </PageContainer>
+    </>
   );
 };
 
-export default LoginPage;
+export default Login;
 
-const LoginContainer = styled.div`
+const PageContainer = styled.div`
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  padding: 100px 0px 0px 0px;
+  height: 100vh;
+  background-color: black;
+`;
+
+const MainContainer = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
-  height: 100%;
-  width: 100%;
-  color: #fff;
-  margin-top: -80px;
+  gap: 10px;
 `;
 
-const Input = styled.input`
-  width: 300px;
-  padding: 10px;
-  margin: 10px 0;
+const StyledInput = styled.input`
+  width: 400px;
+  padding: 15px;
+  border-radius: 8px;
   border: none;
-  border-radius: 5px;
   font-size: 16px;
-  box-sizing: border-box;
 `;
 
-const ErrorMessage = styled.p`
+const SubmitButton = styled.input`
+  width: 430px;
+  padding: 15px;
+  border-radius: 8px;
+  border: none;
+  background-color: ${({ isFormValid }) => (isFormValid ? '#90AFFF' : '#333')};
+  color: ${({ isFormValid }) => (isFormValid ? 'white' : '#999999')};
+  font-size: 16px;
+  cursor: ${({ isFormValid }) => (isFormValid ? 'pointer' : 'not-allowed')};
+  transition: background-color 0.3s ease, color 0.3s ease;
+
+  &:disabled {
+    pointer-events: none;
+  }
+`;
+
+const LoginText = styled.h1`
+  color: white;
+  margin-bottom: 30px;
+`;
+
+const ErrorText = styled.h1`
   color: red;
   font-size: 12px;
-  margin: 0 0 10px;
-`;
-
-const Button = styled.button`
-  width: 300px;
-  padding: 10px;
-  margin-top: 20px;
-  border: none;
-  border-radius: 5px;
-  font-size: 16px;
-  cursor: ${({ isValid }) => (isValid ? 'pointer' : 'not-allowed')};
-  background-color: ${({ isValid }) => (isValid ? '#ff4d6d' : 'gray')}; /* 활성화된 경우 진한 핑크색 */
-  color: #fff;
-  transition: background-color 0.3s ease;
 `;
